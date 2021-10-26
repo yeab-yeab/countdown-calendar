929d8c865763360fa41c8ba0846e6a53682511d8
yeabsera dessalegn <71721807+yeab-yeab@users.noreply.github.com>
2021-10-26 15:14:58 +0300

    Create countdown calendar.py

---
+++ countdown calendar.py
@@ -0,0 +1,41 @@
+from datetime import date, datetime
+from tkinter import Canvas, Tk
+
+def get_events():
+    list_events = []
+    with open('events.txt') as file:
+        for line in file:
+            line = line.rstrip('\n')
+            current_event = line.split(',')
+            event_date = datetime.strptime(current_event[1], '%d/%m/%y').date()
+            current_event[1] = event_date
+            list_events.append(current_event)
+    return list_events
+
+def days_between_dates(date1, date2):
+    time_between = str(date1 - date2)
+    number_of_days = time_between.split(' ')
+    return number_of_days[0]
+
+root = Tk()
+c = Canvas(root, width=800, height=800, bg='green')
+c.pack()
+c.create_text(100, 50, anchor='w', fill='pink', font='Courier 36 bold underline', text='My Countdown Calendar')
+events = get_events()
+today = date.today()
+
+vertical_space = 120
+events.sort(key=lambda x: x[1])
+
+for event in events:
+    event_name = event[0]
+    days_until = days_between_dates(event[1], today)
+    display = 'It is %s days until %s' % (days_until, event_name)
+    if (int(days_until) <= 7):
+        text_col = 'red'
+    else:
+        text_col = 'lightblue'
+    c.create_text(100, vertical_space, anchor='w', fill=text_col, font='Arial 28 bold', text=display)
+    vertical_space = vertical_space + 50
+
+root.mainloop()
